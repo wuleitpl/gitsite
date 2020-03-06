0a727b2aa44a6f4c4ebbbd551dd3b8c3e8379986
Wu Lei <626431065@qq.com>
2020-03-06 10:05:22 +0800

    Add flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,10 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world! Hello from Flask! This is the first web app built by wulei.'
