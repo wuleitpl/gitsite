905d3d8eb498f6b179a049ea5283b757fcc3a588
Wu Lei <626431065@qq.com>
2020-03-06 10:45:35 +0800

    Edit flask_app.py on dell

--- flask_app.py
+++ flask_app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world! Hello from Flask! This is the first web app built by wulei.'
+    return 'Hello world! Hello from Flask! This is the first web app built by wulei. Edit on dell'
