2d30572b81a92763ea3689f16c244aa213e35b23
Wu Lei <626431065@qq.com>
2020-03-06 19:59:13 +0800

    Edit app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'Hello workd! from app import app as application'
+    return 'Hello world! from app import app as application'
